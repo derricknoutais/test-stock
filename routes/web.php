50e381d6f43968c74774af61d1b856cd3b524e73
Derrick Noutais <derricknoutais@gmail.com>
2020-03-30 11:54:37 +0100

    fournisseur

--- routes/web.php
+++ routes/web.php
@@ -390,7 +390,7 @@ Route::put('product-template', function ( Request $request) {
 });
 Route::post('product-fournisseur', function(Request $request){
 
-    $product = Product::where('id', $request['product']['id'])->find();
+    $product = Product::where('id', $request['product']['id'])->first();
     $found = DB::table('product_fournisseur')->where( ['product_id' => $product->id ])->delete();
     foreach($request['product']['fournisseurs'] as $fournisseur){
         DB::table('product_fournisseur')->insert([
