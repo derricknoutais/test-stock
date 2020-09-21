a669fc533e61e05e9e827d044c73985c1728e16a
Derrick Noutais <derricknoutais@gmail.com>
2020-09-21 10:39:17 +0100

    Fixed Nouveaux Produits dans Facture Show

--- resources/views/facture/index.blade.php
+++ resources/views/facture/index.blade.php
@@ -3,31 +3,37 @@
 
 @section('content')
     <facture :data="{{ $factures }}" inline-template>
-        <div>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Nom</th>
-                        <th>Montant Total</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="facture in data">
-                        <td scope="row">
-                            <a :href="'/facture/' + facture.id ">
-                                @{{ facture.nom }}
-                            </a>
-                        </td>
-                        <td>@{{ total(facture) }}</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td scope="row"></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
+        <div class="tw-container tw-mx-auto">
+            <div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Nom</th>
+                            <th>Montant Total</th>
+                            <th>Bon Livraison</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="facture in data">
+                            <td scope="row">
+                                <a :href="'/facture/' + facture.id ">
+                                    @{{ facture.nom }}
+                                </a>
+                            </td>
+                            <td>@{{ total(facture) }}</td>
+                            <td v-if="facture.bon_livraison_id">
+                                <a v-if="bc.bon_livraison_id" class="tw-btn tw-btn-dark"
+                                    :href="'https://stapog.vendhq.com/consignment/' + bc.bon_livraison_id">Voir B.L dans Vend</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td scope="row"></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </facture>
 @endsection
