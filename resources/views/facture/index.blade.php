014326891e6d4a62b9d49703da58512a24bb5f56
Derrick Noutais <derricknoutais@gmail.com>
2020-12-07 15:00:31 +0100

    Factures

--- resources/views/facture/index.blade.php
+++ resources/views/facture/index.blade.php
@@ -21,7 +21,7 @@
                                 </a>
                             </td>
                             <td>@{{ total(facture) }}</td>
-                            <td v-if="facture.bon_livraison_id">
+                            <td >
                                 <a v-if="facture.bon_livraison_id" class="tw-btn tw-btn-dark"
                                     :href="'https://stapog.vendhq.com/consignment/' + facture.bon_livraison_id" target="_blank">Voir B.L dans Vend</a>
                             </td>
