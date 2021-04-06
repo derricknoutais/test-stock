4d2faf6cf4c1e019014f9bc658196a297b64185d
Derrick <derricknoutais@gmail.com>
2021-04-06 12:29:48 +0100

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,7 +9,7 @@ use App\Fournisseur;
 class ProductController extends Controller
 {
     public function index(){
-        $products = Product::with('fournisseurs', 'handle')->orderBy('handle')->get();
+        $products = Product::with('fournisseurs', 'handle')->orderBy('handle_id')->get();
         $fournisseurs = Fournisseur::orderBy('nom')->get();
         return view('products.index',compact('products', 'fournisseurs'));
     }
