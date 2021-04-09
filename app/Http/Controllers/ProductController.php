782ac0caf8ee29afccc38fe89b15be99a794ff94
Derrick <derricknoutais@gmail.com>
2021-04-09 09:06:59 +0100

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,13 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Product;
 use App\Fournisseur;
+use App\Handle;
 
 class ProductController extends Controller
 {
-    public function index(){
-        $products = Product::with('fournisseurs', 'handle')->orderBy('handle_id')->get();
+    public function index(Request $request){
+        if(isset($request->handle)){
+            $products = Product::where('handle_id', $request->handle )->with('fournisseurs')->get();
+        } else {
+            $products = Product::with('fournisseurs')->get();
+        }
+        $handles = Handle::all();
         $fournisseurs = Fournisseur::orderBy('nom')->get();
-        return view('products.index',compact('products', 'fournisseurs'));
+
+        return view('products.index',compact('products', 'fournisseurs', 'handles'));
     }
 
 }
